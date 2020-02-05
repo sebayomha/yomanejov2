7cfcfcea98b6d858860fc61509be339a776b70d7
Sebastian Yomha <sebastian.yomha@synapse.com>
2020-02-04 21:43:01 -0300

    tooltip style

--- src/app/components/schedule-tooltip/schedule-tooltip.component.ts
+++ src/app/components/schedule-tooltip/schedule-tooltip.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-
 declare var $:any;
 
 @Component({
@@ -12,25 +11,15 @@ export class ScheduleTooltipComponent {
 
     @Input() id_car:number;
     @Input() info_schedule_car: any;
-    items
-    tooltipString = '';
+    tooltipString: string = '';
     constructor() { }
 
     ngOnInit() {
-        console.log('CRONOG',this.info_schedule_car);
-
-        this.items = ['1','2'];
-
-        this.tooltipString = '<br><ul style="margin-bottom: 0px;">';
+        this.tooltipString = '<br><ul>';
         this.info_schedule_car.forEach(element => {
             this.tooltipString = this.tooltipString.concat('<li>Alumno: ' + element.alumno +'<br>Dire: '+ element.direccion +'<br> Horario: '+ element.horario+ ' hs</li><br>');
         });
 
         this.tooltipString = this.tooltipString.concat('</ul>')
-
-        console.log(this.tooltipString)
      }
-
-     
-
 }
