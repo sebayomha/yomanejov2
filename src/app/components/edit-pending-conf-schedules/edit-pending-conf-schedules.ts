e5c60292a9a3e863aff3d03fdc69d25c4fc42436
unknown <tute.guazzaroni@gmail.com>
2020-03-01 19:23:30 -0300

    agrego componente de edicion de cronograma pend. estado: NO FUNCIONA

---
+++ src/app/components/edit-pending-conf-schedules/edit-pending-conf-schedules.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'edit-pending-conf-schedules',
+  templateUrl: './edit-pending-conf-schedules.component.html',
+  styleUrls: ['./edit-pending-conf-schedules.component.scss']
+})
+
+export class EditPendingConfSchedules {
+
+  constructor() { }
+
+  ngOnInit() {
+
+   }
+}
