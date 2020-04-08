1efb44bfa6c6ab64f5c9b5c0bee592fd425846ad
Matias Guazzaroni <tute.guazzaroni@gmail.com>
2020-04-07 22:43:48 -0300

    avances autos

--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -1,26 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AutosService } from '../../services/autos/autos.service';
-import { MatTableDataSource } from '@angular/material/table';
+import { Response } from '../../models/response';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.css']
+  styleUrls: ['./cars.component.css', '../../global-styles.scss']
 })
 export class CarsComponent implements OnInit {
 
+  @ViewChild('customModal') customModal;
+
   autos;
   displayedColumns: string[] = ['Id', 'patente', 'color', 'zona'];
+  agregar_auto_modal:boolean = false;
+  operation: string;
+
 
   constructor(private autosService: AutosService) { }
 
   ngOnInit() {
 
     this.autosService.obtenerAutos().subscribe( (response: Response)=>{
-      this.autos = response;
+      this.autos = response.data;
     });
     
 
+  }
+
+  onCustomModalClose() {
+    this.customModal.onClose();
+  }
+
+  agregarAuto() {
+    this.operation = 'agregar_auto';
+    this.customModal.open();
+  }
+
+  confirmModal() {
+    
   }
 
 }
