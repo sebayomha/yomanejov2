ee18416aa4b7cba24abff3d8282712ae83c6e4f1
Sebastian Yomha <sebastian.yomha@synapse.com>
2020-04-11 14:52:52 -0300

    fix

--- src/app/appConstants.ts
+++ src/app/appConstants.ts
@@ -3,9 +3,10 @@ import { HttpClient } from '@angular/common/http';
 
 export class AppSettings {
     public static http: HttpClient;
-    public static USER_ROLE = new AuthService(AppSettings.http).decodePayload().role;
+    public static isLoggedIn = new AuthService(AppSettings.http).isLoggedIn();
+    public static USER_ROLE = AppSettings.isLoggedIn ? new AuthService(AppSettings.http).decodePayload().role: null;
 
     public static refreshRole() {
-        AppSettings.USER_ROLE = new AuthService(AppSettings.http).decodePayload().role;
+        AppSettings.USER_ROLE =  AppSettings.isLoggedIn ? new AuthService(AppSettings.http).decodePayload().role: null;
     }
 }
